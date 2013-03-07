5d342e4dcc62a0dfe18a3ec320129e23a717d44f
OpenShift guest <a.febrer@yahoo.com.ph>
2013-03-06 21:56:04 -0500

    update-hc-chat

---
+++ app.js
@@ -0,0 +1,66 @@
+
+/**
+ * Module dependencies.
+ */
+
+var express = require('express')
+  , routes = require('./routes')
+  , user = require('./routes/user')
+  , http = require('http')
+  , passport = require ('passport')
+  , path = require('path');
+
+var app = express();
+
+app.configure(function(){
+  app.set('port', process.env.PORT || 3000);
+  app.set('views', __dirname + '/views');
+  app.set('view engine', 'jade');
+  app.use(express.favicon());
+  app.use(express.logger('dev'));
+  app.use(express.bodyParser());
+  app.use(express.methodOverride());
+  app.use(express.cookieParser('hatchcatch'));
+  app.use(express.session({
+        key: "hatchcatch"
+}));
+  app.use(passport.initialize());
+  app.use(passport.session());
+  app.use(app.router);
+  app.use(express.static(path.join(__dirname, 'public')));
+});
+
+app.configure('development', function(){
+  app.use(express.errorHandler());
+});
+
+
+
+app.get('/', routes.index);
+app.get('/option', routes.option);
+app.get('/authfb', passport.authenticate('facebook'));
+app.get('/authtw', passport.authenticate('twitter'));
+
+app.get('/authfb/callback',
+        passport.authenticate('facebook', {
+            succesRedirect: '/option',
+            failureRedirect: '/'
+        })
+);
+
+app.get('/authtw/callback',
+        passport.authenticate('twitter', {
+            succesRedirect: '/option',
+            failureRedirect: '/' 
+        })
+);
+
+app.get('logout', function(req, res) {
+    req.logout();
+    res.Redirect('/');
+});
+
+
+http.createServer(app).listen(app.get('port'), function(){
+  console.log("Express server listening on port " + app.get('port'));
+});
---
+++ routes/index.js
@@ -0,0 +1,12 @@
+
+/*
+ * GET home page.
+ */
+
+exports.index = function(req, res){
+  res.render('login');
+};
+
+exports.index = function(req, res){
+  res.render('option');
+};
